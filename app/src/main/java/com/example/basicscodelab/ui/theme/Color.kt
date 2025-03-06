68f433a17876734ef5840699485b22422e287251
Tu23IT296 <tunt.23itvku.udn.vn>
2025-03-06 22:06:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/basicscodelab/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.example.basicscodelab.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// 🔹 Thêm các màu bị thiếu
+val Navy = Color(0xFF073042)       // Màu xanh đậm
+val Chartreuse = Color(0xFFEFF7CF) // Màu xanh lá nhạt
+val LightBlue = Color(0xFF4285F4)  // Màu xanh dương nhạt
